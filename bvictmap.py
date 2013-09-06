4f684d1a8b9b369e3323bdbd9382841e0a990193
Blevene <blevene@salesforce.com>
2013-09-06 09:51:31 -0400

    Update bvictmap.py
    Added comments

--- bvictmap.py
+++ bvictmap.py
@@ -1,6 +1,8 @@
 #!/bin/env/pyton
 # Written by: Blevene
-# Make sure you set your default
+# Make sure you set your default DNS to OpenDNS
+# or google, otherwise your ISP will ::mad::
+# top-1m.csv is sourced from alexa top 1kk
 
 import csv
 import adns
@@ -14,7 +16,7 @@ for row in reader:
 
 writer = csv.writer(open('plzworkresults.csv', 'wb')) 
 resolvdict = {}
-count = 0
+count = 0 # Iterator to print to console within for loop
 for key, value in alexadict.iteritems():
     try:
         resolved = c.synchronous(value, adns.rr.A)

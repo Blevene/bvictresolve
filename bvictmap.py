1a3b9bd895e35cef6b098328e86286b7b1865635
Blevene <blevene@salesforce.com>
2013-09-08 17:09:00 -0400

    Update bvictmap.py

--- bvictmap.py
+++ bvictmap.py
@@ -1,30 +1,32 @@
 #!/bin/env/python
-# Written by: Blevene
-# Make sure you set your default DNS to OpenDNS
-# or google, otherwise your ISP will ::mad::
-# top-1m.csv is sourced from alexa top 1kk
 
 import csv
 import adns
+import os.path
 c=adns.init()
 
-reader = csv.reader(open('top-1m.csv', 'r'))
-alexadict = {}
-for row in reader:
-    k, v = row
-    alexadict[k] = v
-
-writer = csv.writer(open('plzworkresults.csv', 'wb')) 
-resolvdict = {}
-count = 0 # Iterator to print to console within for loop
-for key, value in alexadict.iteritems():
-    try:
-        resolved = c.synchronous(value, adns.rr.A)
-        resolvdict.setdefault(key, [value]).append(resolved[3])
-        for key, value in resolvdict.iteritems():
-			writer.writerow([key, value])
+#Check if results file exists already.
+if os.path.isfile('plzworkresults.csv'):
+	print "BUTTS"
+# If no results file, resolve all of the domains in top-1m.csv
+else:
+	reader = csv.reader(open('top-1m.csv', 'r'))
+	alexadict = {}
+	for row in reader:
+		k, v = row
+		alexadict[k] = v
+	writer = csv.writer(open('plzworkresults.csv', 'wb')) 
 	resolvdict = {}
-        count += 1
-        print "We've resolved %d domains." % count
-    except IOError:
-        pass
+	count = 0
+	for key, value in alexadict.iteritems():
+	    try:
+	        resolved = c.synchronous(value, adns.rr.A)
+	        resolvdict.setdefault(key, [value]).append(resolved[3])
+	        for key, value in resolvdict.iteritems():
+				writer.writerow([key, value])
+		resolvdict = {}
+	        count += 1
+	        print "We've resolved %d domains." % count
+	    except IOError:
+	        pass
+        

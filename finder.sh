e210b0f7d72f022a7ad62d311627244db96c25cf
Blevene <blevene@salesforce.com>
2013-09-09 09:17:06 -0400

    Update finder.sh

--- finder.sh
+++ finder.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # where $1 is the results and $2 is a list of targets
-# $3 is your final results (overlaps)
+# redirect the results to a file of your choice
 while read -r p; do
-	grep $p $1 | grep -v "127.0.0.1" | sort -n | uniq > $3
+	cat $1 | grep $p | grep -v "127.0.0.1" | sort -n | uniq
 done < $2

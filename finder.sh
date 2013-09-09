a1a7ab49b33226da20a9e7e09e0370dfcda44327
Blevene <blevene@salesforce.com>
2013-09-09 01:38:48 -0400

    Update finder.sh

--- finder.sh
+++ finder.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # where $1 is the results and $2 is a list of targets
-while read p; do
-	cat $1 | grep $p | grep -v "127.0.0.1" | sort -n | uniq
+# $3 is the name of a results file, csv for best view
+while read -r p; do
+	grep $p $1 | grep -v "127.0.0.1" > $3 | sort -n | uniq
 done < $2
